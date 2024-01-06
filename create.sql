ca839a87618c568b2b220738eb87ea772bcc5839
yanAklog <91112454+yanAklog@users.noreply.github.com>
2024-01-06 13:15:30 +0200

    Add files via upload

---
+++ create.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Genre
+(
+  genre_type VARCHAR(50) NOT NULL,
+  PRIMARY KEY (genre_type)
+);
+
+CREATE TABLE Category
+(
+  category_name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (category_name)
+);
+
+CREATE TABLE App
+(
+  app_name VARCHAR(250) NOT NULL,
+  app_size INT,
+  app_price INT,
+  content_rating VARCHAR(50) NOT NULL,
+  category_name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (app_name),
+  FOREIGN KEY (category_name) REFERENCES Category(category_name)
+);
+
+CREATE TABLE App_Genre
+(
+  app_name VARCHAR(250) NOT NULL,
+  genre_type VARCHAR(50) NOT NULL,
+  PRIMARY KEY (app_name, genre_type),
+  FOREIGN KEY (app_name) REFERENCES App(app_name),
+  FOREIGN KEY (genre_type) REFERENCES Genre(genre_type)
+);
